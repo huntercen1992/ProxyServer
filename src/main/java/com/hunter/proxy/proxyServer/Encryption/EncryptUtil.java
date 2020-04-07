90c7996bdefc7d96d645bdfe25e3ee7d9b26ce6c
huntercen@outlook.com <huntercen@outlook.com>
2020-04-07 15:26:24 +0800

    e

--- src/main/java/com/hunter/proxy/proxyServer/Encryption/EncryptUtil.java
+++ src/main/java/com/hunter/proxy/proxyServer/Encryption/EncryptUtil.java
@@ -542,7 +542,7 @@ public class EncryptUtil {
 		if(flag)return resource;
 		if(resource == null)return null;
 
-		for(int i = 0; i < resource.length; i++){
+		for(int i = 0; i < resource.length; i=i+10){
 			resource[i] = encrypMap.get(resource[i]).byteValue();
 		}
 		return resource;
@@ -552,7 +552,7 @@ public class EncryptUtil {
 		if(flag)return resource;
 		if(resource == null)return null;
 
-		for(int i = 0; i < resource.length; i++){
+		for(int i = 0; i < resource.length; i=i+10){
 			resource[i] = decrypMap.get(resource[i]).byteValue();			
 		}
 		return resource;
@@ -580,10 +580,18 @@ public class EncryptUtil {
 	}
 	
 	public static void main(String[] s){
-		byte[] resource = {1,6,4,9,32,125,13,-32,1,-45,-124,-102,-53,35};
+		byte[] resource = {57, 22, -91, 82, -76, -126, -107, -28, 74, -96, 30, -51, 121, -92, -39, 45, -128, -10, 122, 86, -112, 105, -73, -87, 95, 5, 90,
+				 87, -36, -67, -118, -44, -69, 72, 51, 78, 16, -83, -56, -35, -71, -85, 7, -121, 113, -123, 49, 116, 19, 18, 110, 75, -57, 55, -30,
+				 85, -109, 70, -23, -120, 50, -119, -89, -99, 39, -12, -32, -115, -38, 59, 79, -17, 115, 0, 114, -43, 33, -9, 17, -106, -53, -65,
+				 -75, 65, -81, -66, 29, 60, -16, 63, -90, 107, -34, 112, -2, 126, -127, -54, 14, -41, 125, -47, 104, 69, 34, 31, -62, 97, 73, 124,
+				 -113, 10, 36, 98, 84, 2, 93, -55, 9, -45, -105, 13, 123, -48, 1, -5, -86, 44, -15, -74, 111, 108, 64, -13, 58, 118, 66, -18, 56,
+				 25, 127, -124, 41, -78, 53, -111, 38, 21, 6, 40, 120, -68, 76, -116, 12, -79, -31, -14, -24, -49, -104, -95, 81, -125, -114, -27,
+				 -64, 92, 91, -11, 77, -108, 43, -98, 37, 119, -77, 8, -40, -94, 62, 3, 47, 61, -58, -61, 68, -29, -46, 96, -101, 102, -82, 80, 42,
+				 -103, -117, -20, -7, -21, 24, 89, 83, -122, -70, -8, 26, 20, 100, 15, 27, -19, 101, 99, 28, -110, 11, -42, 54, -50, 35, -4, 106,
+				 46, -60, -88, 103, -97, -80, -37, -52, -84, 94, 52, -6, 4, -93, 109, 71, 117, -59, -72, -102, 48, -25, -22, 88, 32, -63, -100, 23,
+				 -1, -3, -33, -26, 67};
 		resource = EncryptUtil.encrypt(resource);
 		resource = EncryptUtil.decrypt(resource);
-		System.out.print(resource);
 		for(byte b : resource){
 			System.out.print(b+ ",");
 		}
